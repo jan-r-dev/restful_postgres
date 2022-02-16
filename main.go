1e5390d3ed4e75b28fd68e7e3b11a9d6c8e3324c
jan <jan@localhost.localdomain>
2022-02-16 21:06:32 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
+func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("POSTGRES_DB"))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Connected to DB", conn)
+}
