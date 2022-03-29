b75430b73bfa635ece7d1fb4a5f3a93ff53d20e9
jan <jan@localhost.localdomain>
2022-03-29 20:06:18 +0200

    article proof of concept

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 
@@ -10,92 +9,43 @@ import (
 )
 
 type article struct {
-	Pk           int
-	Title        string
-	Text_content []string
-	Image_url    []string
-	Snippet_url  []string
-	Source_url   []string
+	Pk           int      `json:"pk,omitempty"`
+	Title        string   `json:"title,omitempty"`
+	Text_content []string `json:"text,omitempty"`
+	Image_url    []string `json:"images,omitempty"`
+	Snippet_url  []string `json:"snippets,omitempty"`
+	Source_url   []string `json:"sources,omitempty"`
 }
 
-var querystringArticle string = `
-select article.pk, title, text_content, image_url, snippet_url, source_url from article
+func postgres(ctx context.Context, articleID string) (article, error) {
+	qs := "select article.pk, title, text_content, image_url, snippet_url, source_url from article inner join title on article.title_fk = title.pk where article.pk = " + articleID
 
-inner join title on article.title_fk = title.pk
-`
-var querystringProject string = `
-select project.pk, title, summary, url, created_date, stack from project
+	article := article{}
 
-inner join title on project.title_fk = title.pk
-`
-
-// TO DO
-/*
-1/ Create project struct
-2/ Examine the option of employing a sort of manifest that would allow for dynamic struct assignment in the row read
-2/ Create functions for
-	Querying project
-	Processing project rows
-
-*/
-// TO DO ENDS
-func main() {
-	c := context.Background()
-
-	conn := connect(c)
-	defer conn.Close(c)
-
-	fmt.Println("Query for article or project: ")
-	var choice string
-	fmt.Scanln(&choice)
-
-	// Should be separate func that returns rows instead
-	var rows pgx.Rows
-	var bs [][]byte
-	if choice == "a" {
-		rows = queryArticle(conn, c, querystringArticle)
-		bs = rowsArticle(rows)
-	} else if choice == "p" {
-		// currently placeholder
-		rows = queryArticle(conn, c, querystringProject)
-		bs = rowsProject(rows)
+	conn, errConn := pgx.Connect(ctx, os.Getenv("POSTGRES_DB"))
+	if errConn != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", errConn)
+		conn.Close(ctx)
+		return article, errConn
 	}
-	// Separate func end
+	defer conn.Close(ctx)
 
+	rows, errQuery := conn.Query(ctx, qs)
+	if errQuery != nil {
+		fmt.Fprintf(os.Stderr, "Error while executing query: %v\n", errQuery)
+		rows.Close()
+		return article, errQuery
+	}
 	defer rows.Close()
 
-	if rows.Err() != nil {
-		fmt.Fprintf(os.Stderr, "Error while reading rows: %v\n", rows.Err())
-	}
-
-	fmt.Println(len(bs[0]))
+	return readRowsArticle(rows)
 }
 
-func connect(c context.Context) *pgx.Conn {
-	conn, err := pgx.Connect(c, os.Getenv("POSTGRES_DB"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-
-	return conn
-}
-
-func queryArticle(conn *pgx.Conn, c context.Context, q string) pgx.Rows {
-	rows, err := conn.Query(c, q)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while executing query: %v\n", err)
-	}
-
-	return rows
-}
-
-func rowsArticle(rows pgx.Rows) [][]byte {
-	bsj := [][]byte{}
-
+func readRowsArticle(rows pgx.Rows) (article, error) {
+	a := article{}
 	for rows.Next() {
-		a := article{}
-		errS := rows.Scan(
+
+		errScan := rows.Scan(
 			&a.Pk,
 			&a.Title,
 			&a.Text_content,
@@ -103,46 +53,11 @@ func rowsArticle(rows pgx.Rows) [][]byte {
 			&a.Snippet_url,
 			&a.Source_url,
 		)
-		if errS != nil {
-			fmt.Fprintf(os.Stderr, "Error while scanning rows: %v\n", errS)
+		if errScan != nil {
+			fmt.Fprintf(os.Stderr, "Error while scanning rows: %v\n", errScan)
+			return a, errScan
 		}
-
-		bs, errM := json.Marshal(a)
-		if errM != nil {
-			fmt.Fprintf(os.Stderr, "Error while marshaling json: %v\n", errM)
-		}
-
-		bsj = append(bsj, bs)
 	}
 
-	return bsj
-}
-
-func rowsProject(rows pgx.Rows) [][]byte {
-	// Placeholder
-	bsj := [][]byte{}
-
-	for rows.Next() {
-		a := article{}
-		errS := rows.Scan(
-			&a.Pk,
-			&a.Title,
-			&a.Text_content,
-			&a.Image_url,
-			&a.Snippet_url,
-			&a.Source_url,
-		)
-		if errS != nil {
-			fmt.Fprintf(os.Stderr, "Error while scanning rows: %v\n", errS)
-		}
-
-		bs, errM := json.Marshal(a)
-		if errM != nil {
-			fmt.Fprintf(os.Stderr, "Error while marshaling json: %v\n", errM)
-		}
-
-		bsj = append(bsj, bs)
-	}
-
-	return bsj
+	return a, nil
 }
